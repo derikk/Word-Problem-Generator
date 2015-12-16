d8feb44753af030678fa319ed06e4382ad45a4ea
AG8 <andrew2000g@gmail.com>
2015-12-16 10:04:17 -0500

    Update common.js

--- common.js
+++ common.js
@@ -4,6 +4,11 @@ function randInt(min, max) {
 	return Math.floor(Math.random() * (max - min)) + min;
 }
 
+//Get a random arbitrary number, rounded to 2 decimal digits
+function getRandomArbitrary(min, max) {
+    return +(Math.random() * (max - min) + min).toFixed(2);
+}
+
 // Reduce a fraction by finding the Greatest Common Divisor and dividing by it.
 // Credit: http://stackoverflow.com/questions/4652468/is-there-a-javascript-function-that-reduces-a-fraction
 function reduce(numerator, denominator) {
